4d7fff35b48ecb3a0a364640b379deedc1f19fad
camila <camilailiberg@gmail.com>
2021-06-10 02:03:02 -0400

    all working but niw trying to get the load more to work

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ console.log("page loaded");
 const apiKey = "3e5775f6559acd9fc0c12c6586928764";
 const baseURL = "https://api.themoviedb.org/3/";
 
+let page = 1;
+
 const movieForm = document.querySelector("#movieForm");
 const movieContent = document.querySelector(".content");
+const typeShowing = document.querySelector(".type-showing");
 
 movieForm.addEventListener("submit", getConfig);
+
+function loadMore() {
+	page = page + 1;
+}
 
 async function getConfig(event) {
 	event.preventDefault(); // prevents page from reloading
@@ -21,11 +28,14 @@ async function getConfig(event) {
 	const baseImageURL = responseData.images.secure_base_url;
 	const configData = responseData.images;
 
-	// console.log("config:", configData);
 	console.log("config fetched");
 
 	const movieInput = event.target.movieName;
 	const movie = movieInput.value;
+
+	typeShowing.innerHTML = `
+    You Searched For:  '${movie}'
+    `;
 
 	console.log("running for movie:", movie);
 
@@ -38,16 +48,22 @@ async function runSearch(movie, baseImageURL) {
 	const responseSearch = await fetch(searchURL);
 	const searchData = await responseSearch.json();
 
-	// movieContent.innerHTML = JSON.stringify(searchData, null, 4);
-
 	movieContent.innerHTML = ``;
 
 	searchData.results.forEach((element) => {
 		let image = `${baseImageURL}original${element.poster_path}`;
-		// console.log(image);
+
 		movieContent.innerHTML += `
+        <div class="individial-movie">
             <img class="moviePoster" src="${image}" alt="${element.title}" />
-            <!-- <h3>${element.title}</h3> -->
+            <div class="name-rating">
+                <h3>${element.title}</h3>
+                <div class="star-and-rating">
+                    <img class="star-rating" src="/images/star.png" alt="star-rating" />
+                    <h3>${element.vote_average}</h3>
+                </div>
+            </div>
+        </div>
     `;
 	});
 }
@@ -55,6 +71,10 @@ async function runSearch(movie, baseImageURL) {
 // document.addEventListener("DOMContentLoaded", getConfig);
 
 window.onload = async function (event) {
+	typeShowing.innerHTML = `
+    Now Playing
+    `;
+
 	const configURL = `${baseURL}configuration?api_key=${apiKey}`;
 
 	const configResponse = await fetch(configURL);
@@ -64,7 +84,7 @@ window.onload = async function (event) {
 
 	const baseImageURL = configResponseData.images.secure_base_url;
 
-	const nowPlayingApiURL = `${baseURL}movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
+	const nowPlayingApiURL = `${baseURL}movie/now_playing?api_key=${apiKey}&language=en-US&page=${page}`;
 
 	const response = await fetch(nowPlayingApiURL);
 	const responseData = await response.json();
@@ -74,8 +94,16 @@ window.onload = async function (event) {
 	responseData.results.forEach((element) => {
 		let image = `${baseImageURL}original${element.poster_path}`;
 		movieContent.innerHTML += `
-        <img class="moviePoster" src="${image}" alt="${element.title}" />
-        <!-- <h3>${element.title}</h3> -->
+        <div class="individial-movie">
+            <img class="moviePoster" src="${image}" alt="${element.title}" />
+            <div class="name-rating">
+                <h3>${element.title}</h3>
+                <div class="star-and-rating">
+                    <img class="star-rating" src="/images/star.png" alt="star-rating" />
+                    <h3>${element.vote_average}</h3>
+                </div>
+            </div>
+        </div>
 		`;
 	});
 };
