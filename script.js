18738efbd8200c66be4f137023314bd4ed54411c
camila <camilailiberg@gmail.com>
2021-06-10 00:49:15 -0400

    I can search movies and posters show up

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
 console.log("page loaded");
 
 const apiKey = "3e5775f6559acd9fc0c12c6586928764";
+const baseURL = "https://api.themoviedb.org/3/";
 
-const movieForm = document.querySelector(".movieForm");
+const movieForm = document.querySelector("#movieForm");
 const movieContent = document.querySelector(".content");
-const individualMovie = document.querySelector(".individual-movie");
-const movieTitleAndRating = document.querySelector(".title-and-rating");
 
-window.onload = async function (event) {
-	const nowPlayingApiURL =
-		"https://api.themoviedb.org/3/movie/now_playing?api_key=3e5775f6559acd9fc0c12c6586928764&language=en-US&page=1";
+movieForm.addEventListener("submit", getConfig);
 
-	const response = await fetch(nowPlayingApiURL);
+async function getConfig(event) {
+	event.preventDefault(); // prevents page from reloading
+
+	const configURL = `${baseURL}configuration?api_key=${apiKey}`;
+
+	const response = await fetch(configURL);
 	const responseData = await response.json();
 
 	console.log(responseData);
 
-	responseData.results.forEach((element) => {
-		individualMovie.innerHTML += `
-        <img class="moviePoster" src="${element.poster_path}" alt="${element.title}" />
-        <h3>${element.title}</h3>
-		`;
-		movieTitleAndRating.innerHTML += `
-        <h3>${element.title}</h3>
-        <h3>Rating<h3>
-        `;
-	});
-};
+	const baseImageURL = responseData.images.secure_base_url;
+	const configData = responseData.images;
 
-function loadAll() {}
+	// console.log("config:", configData);
+	console.log("config fetched");
+
+	const movieInput = event.target.movieName;
+	const movie = movieInput.value;
+
+	console.log("running for movie:", movie);
+
+	runSearch(movie, baseImageURL);
+}
+
+async function runSearch(movie, baseImageURL) {
+	const searchURL = `${baseURL}search/movie?api_key=${apiKey}&language=en-US&query=${movie}`;
+
+	const responseSearch = await fetch(searchURL);
+	const searchData = await responseSearch.json();
+
+	// movieContent.innerHTML = JSON.stringify(searchData, null, 4);
+
+	movieContent.innerHTML = ``;
+
+	searchData.results.forEach((element) => {
+		let image = `${baseImageURL}original${element.poster_path}`;
+		// console.log(image);
+		movieContent.innerHTML += `
+            <img class="moviePoster" src="${image}" alt="${element.title}" />
+            <!-- <h3>${element.title}</h3> -->
+    `;
+	});
+}
+
+// document.addEventListener("DOMContentLoaded", getConfig);
+
+// window.onload = async function (event) {
+// 	const nowPlayingApiURL = `${baseURL}movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
+
+// 	const response = await fetch(nowPlayingApiURL);
+// 	const responseData = await response.json();
+
+// 	console.log(responseData);
+
+// 	responseData.results.forEach((element) => {
+// 		const image = `https://api.themoviedb.org/3/movie/${element.id}/images?api_key=${apiKey}&language=en-US`;
+// 		console.log(image);
+// 		movieContent.innerHTML += `
+//         <img class="moviePoster" src="${image}" alt="${element.title}" />
+//         <h3>${element.title}</h3>
+// 		`;
+// 	});
+// };
